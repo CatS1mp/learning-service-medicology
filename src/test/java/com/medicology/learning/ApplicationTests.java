8eda5f6554ce46da79e6814d708e3c68df54e25c
KhoaLND <lengocdangkhoak16@siu.edu.vn>
2026-03-23 20:28:36 +0700

    Add initial project structure with Maven configuration and application setup

---
+++ src/test/java/com/medicology/learning/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicology.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
